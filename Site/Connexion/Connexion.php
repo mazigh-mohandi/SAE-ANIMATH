86121cb82faf94e0989b47690ed961b0d97bdf90
aicha-lrt <92796149+aicha-lrt@users.noreply.github.com>
2023-01-09 06:34:37 +0100

    Add files via upload
    script de connexion pas terminé

--- Site/Connexion/Connexion.php
+++ Site/Connexion/Connexion.php
@@ -7,43 +7,46 @@ $m = Model::getModel();
 
 session_start();
 
+//initialisation de variables d'erreurs 
+//on leur donne une valeur nulle par défaut, si le champ est vide alors un msg d'erreur s'affichera
+$mailErr = ""; $mdpErr = "";
 
 if(isset($_POST['connexion'])){
 
-    // on vérifie que le champ "email" n'est pas vide
     if(empty($_POST['email'])){
-        echo "Le champ Email est vide.";
+        $mailErr = "<br>Le champ Email est vide.";
     } else {
-        // on vérifie maintenant si le champ "Mot de passe" n'est pas vide"
         if(empty($_POST['mdp'])){
-            echo "Le champ Mot de passe est vide.";
+            $mdpErr = "<br>Le champ Mot de passe est vide.";
 
-            
+
         } else {
 
             // les champs email & mdp sont bien postés et pas vides, on sécurise les données entrées par l'utilisateur
             $Email = htmlentities($_POST['email'], ENT_QUOTES, "UTF-8"); 
             $MotDePasse = htmlentities($_POST['mdp'], ENT_QUOTES, "UTF-8");
+
+        // PAS FINI A PARTIR DE LA 
+        // MANQUE: MODEL
+
             //on se connecte à la base de données:
-            $mysqli = mysqli_connect("domaine.tld", "nom d'utilisateur", "mot de passe", "base de données");
+            //$mysqli = mysqli_connect("domaine.tld", "nom d'utilisateur", "mot de passe", "base de données");
             //on vérifie que la connexion s'effectue correctement:
-            if(!$mysqli){
-                echo "Erreur de connexion à la base de données.";
-            } else {
+            //if(!$mysqli){
+            //    echo "Erreur de connexion à la base de données.";
+            //} else {
                 //on fait maintenant la requête dans la base de données pour rechercher si ces données existent et correspondent:
                 //si vous avez enregistré le mot de passe en md5() il vous faudra faire la vérification en mettant mdp = '".md5($MotDePasse)."' au lieu de mdp = '".$MotDePasse."'
-                $Requete = mysqli_query($mysqli,"SELECT * FROM membres WHERE mail = '".$Email."' AND mdp = '".$MotDePasse."'");
+            //    $Requete = mysqli_query($mysqli,"SELECT * FROM membres WHERE mail = '".$Email."' AND mdp = '".$MotDePasse."'");
                 //si il y a un résultat, mysqli_num_rows() nous donnera alors 1
                 //si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
-                if(mysqli_num_rows($Requete) == 0) {
-                    echo "Le email ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
-                } else {
-                    //on ouvre la session avec $_SESSION:
-                    //la session peut être appelée différemment et son contenu aussi peut être autre chose que le email
-                    $_SESSION['email'] = $Email;
-                    echo "Vous êtes à présent connecté !";
-                }
-            }
+            //    if(mysqli_num_rows($Requete) == 0) {
+            //CETTE LIGNE EST BONNE        $mailErr "L'email ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
+            //    } else {
+            //        $_SESSION['email'] = $Email;
+            //CETTE LIGNE EST BONNE        echo "Vous êtes à présent connecté! &nbsp; <a href='../Accueil/Accueil.html'>Retourner à l'accueil</a>";
+            //}
+            //}
         }
     }
 }
--- Site/Connexion/fin_html.php
+++ Site/Connexion/fin_html.php
@@ -7,11 +7,13 @@
         <div class="container2">
             <h4><a href =../accueil/accueil.html>Revenir à la page d'accueil</a></h4>
             <h1>Se connecter</h1>
-            
+            <!-- J'ai rajouté deux <span class="err"> pour gérer les erreurs en php -->
             <form action="connexion.php" method="post">
                 <div class="form">
                     <input type="email" name="email" placeholder="Adresse email">
+                        <span class="err"><?php echo $mailErr;?></span>
                     <input type="password" name="mdp" placeholder="Mot de passe">
+                        <span class="err"><?php echo $mdpErr;?></span>
                     <input type="submit" name="connexion" value="Se connecter">
                 </div>
             </form>
