5b8ba440f595cdf5cccb05d005911009932c7c23
aicha-lrt <92796149+aicha-lrt@users.noreply.github.com>
2023-01-17 16:26:27 +0100

    Update Verify_connexion.php

--- Site/Accueil/Verify_connexion.php
+++ Site/Accueil/Verify_connexion.php
@@ -1,17 +1,20 @@
 <?php
-
-    if($_SESSION['role']==2)
-        {
-           echo" <li><a href='../mon_compte(prof)/mon_compte(prof).html'>Mon Compte <i class='fa fa-user'></i></a></li> ";
-        }
-    elseif($_SESSION['role']==1)
-        {
-            echo" <li><a href='../mon_compte(super)/mon_compte(super).html'>Mon Compte <i class='fa fa-user'></i></a></li> ";
-    
-        }
-    elseif($_SESSION['role']==0)
+    if (isset($_SESSION['role']))
     {
-        echo "<li><a href='../Connexion/Connexion.php'>Connexion <i class='fa fa-user'></i></a></li> ";
+        if($_SESSION['role']==2)
+            {
+            echo" <li><a href='../mon_compte(prof)/mon_compte(prof).html'>Mon Compte <i class='fa fa-user'></i></a></li> ";
+            }
+        elseif($_SESSION['role']==1)
+            {
+                echo" <li><a href='../mon_compte(super)/mon_compte(super).html'>Mon Compte <i class='fa fa-user'></i></a></li> ";
+        
+            }
     }
+    else
+    {
+            echo "<li><a href='../Connexion/Connexion.php'>Connexion <i class='fa fa-user'></i></a></li> ";
+    }
+    
 
 ?>
