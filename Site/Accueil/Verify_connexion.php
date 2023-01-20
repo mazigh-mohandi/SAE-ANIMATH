544e4f45bebe4b20115329286174d49bd12f0926
aicha-lrt <92796149+aicha-lrt@users.noreply.github.com>
2023-01-20 10:47:57 +0100

    Update Verify_connexion.php

--- Site/Accueil/Verify_connexion.php
+++ Site/Accueil/Verify_connexion.php
@@ -3,11 +3,11 @@
     {
         if($_SESSION['role']==2)
             {
-            echo" <li><a href='../mon_compte(prof)/mon_compte(prof).html'>Mon Compte <i class='fa fa-user'></i></a></li> ";
+            echo" <li><a href='../mon_compte(prof)/mon_compte(prof).php'>Mon Compte <i class='fa fa-user'></i></a></li> ";
             }
         elseif($_SESSION['role']==1)
             {
-                echo" <li><a href='../mon_compte(super)/mon_compte(super).html'>Mon Compte <i class='fa fa-user'></i></a></li> ";
+                echo" <li><a href='../mon_compte(super)/mon_compte(super).php'>Mon Compte <i class='fa fa-user'></i></a></li> ";
         
             }
         else
